5aec65c2e87393eb2afa5e63fb11a89388b38b8d
jacr1975 <jacr1975manizales@yahoo.com>
2022-05-13 08:42:19 -0500

    ini2

--- src/mprustand/Mprustand.java
+++ src/mprustand/Mprustand.java
@@ -15,7 +15,7 @@ public class Mprustand {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          System.out.println("Hola mundos...");
+          System.out.println("Hola mundos22...");
     }
     
 }
